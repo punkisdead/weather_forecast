05e3520baf5236e9930770d31e112ed915dd9f4d
Jeremy Anderson <jeremy.d.anderson@gmail.com>
2024-10-21 17:29:35 -0400

    Modify view template

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def index
     @search_param = params[:query]
+    @result = SearchWeather.call(address: @search_param) if @search_param
   end
 
 end
